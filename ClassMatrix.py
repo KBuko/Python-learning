15fdde81503a6d7225935e1e1154daae19428bcb
Kate Buko <katsiaryna.buko@gmail.com>
2022-04-20 15:25:44 +0200

    changes: some methods of class Matrix was changed to class methods and static methods

--- ClassMatrix.py
+++ ClassMatrix.py
@@ -14,6 +14,30 @@ class Matrix:
     matrix_counter_diagonal = 0
     matrix_counter_other = 0
 
+    @classmethod
+    def matrix_counter(cls):
+        cls.matrix_counter_general += 1
+
+    @classmethod
+    def matrix_type_counters(cls, a, b):  # 'a' would be list_diagonal, b - list_other
+        if b == a == {0}:
+            print('Zero matrix')
+            cls.matrix_counter_zero += 1
+        elif a == {1} and b == {0}:
+            print('Identity matrix')
+            cls.matrix_counter_identity += 1
+        elif b == {0} and a != {0}:
+            print('Diagonal matrix')
+            cls.matrix_counter_diagonal += 1
+        else:
+            print('Mixed matrix')
+            cls.matrix_counter_other += 1
+
+    @staticmethod
+    def output(matrix):
+        for i in matrix:
+            print(i)
+
     def new_matrix(self, rows, cols):
         self.rows = rows  # the number of lists inside the main list
         self.cols = cols  # the number of elements inside lists
@@ -29,11 +53,8 @@ class Matrix:
             self.elems.append(self.row_m)
             rows_i += 1
             cols_i = 0
-        Matrix.matrix_counter_general += 1
-
-    def output(self):
-        for i in self.elems:
-            print(i)
+        Matrix.matrix_counter()
+        Matrix.output(self.elems)
 
     def define_matrix(self):
         list_other = []  # the list for elements off diagonal
@@ -47,29 +68,16 @@ class Matrix:
 
         list_diagonal, list_other = set(list_diagonal), set(list_other)
 
-        if list_other == list_diagonal == {0}:
-            print('Zero matrix')
-            Matrix.matrix_counter_zero += 1
-        elif list_diagonal == {1} and list_other == {0}:
-            print('Identity matrix')
-            Matrix.matrix_counter_identity += 1
-        elif list_other == {0} and list_diagonal != {0}:
-            print('Diagonal matrix')
-            Matrix.matrix_counter_diagonal += 1
-        else:
-            print('Mixed matrix')
-            Matrix.matrix_counter_other += 1
+        Matrix.matrix_type_counters(list_diagonal, list_other)
 
 
 # Creating a new object. It can be any number of objects of different sizes.
 myMatrix = Matrix()
 myMatrix.new_matrix(3, 3)
-myMatrix.output()
 myMatrix.define_matrix()
 
 myMatrix2 = Matrix()
 myMatrix2.new_matrix(2, 2)
-myMatrix2.output()
 myMatrix2.define_matrix()
 
 print(f'The total number of matrices you created: {Matrix.matrix_counter_general}\n'
